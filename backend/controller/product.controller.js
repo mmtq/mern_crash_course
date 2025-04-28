a0d55ac4e8c151b8d30623296e8213e53950649c
mmtq <mirtarhimul@gmail.com>
2025-04-28 16:01:16 +0600

    init react and chakra ui

--- backend/controller/product.controller.js
+++ backend/controller/product.controller.js
@@ -31,12 +31,15 @@ export const createProduct = async (req, res) => {
 
 export const deleteProduct = async (req, res) => {
     const {id} = req.params;
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).json({success: false, message: 'Invalid product ID'});
+    }
     try {
         await Product.findByIdAndDelete(id);
         res.status(200).json({success: true, message: 'Product deleted successfully'});
     } catch (error) {
         console.error(error);
-        res.status(500).json({success: false, message: 'Product not found'});
+        res.status(500).json({success: false, message: 'Server error'});
     }
 }
 
--- backend/server.js
+++ backend/server.js
@@ -8,13 +8,14 @@ import productRoutes from './routes/product.route.js';
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use("/api/products", productRoutes);
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     connectDB();
-    console.log('Server started at http://localhost:3000');
+    console.log('Server started at http://localhost:' + PORT);
 });
 
 app.get('/', (req, res) => {
